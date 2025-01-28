1903c3209a82547fc035259df4241105cb4d13f3
Hugo Tachoires <hugo.tachoires@gmail.com>
2025-01-28 11:44:34 +0100

    test: add return test template for mock

---
+++ Speedex.Domain.Tests.Unit/Returns/CreateReturn/CreateReturnTests.cs
@@ -0,0 +1,17 @@
+namespace Speedex.Domain.Tests.Unit.Returns.CreateReturn;
+
+public class CreateReturnTests
+{
+    [Fact]
+    public void Handle_Should_Return_Success_Result_When_Return_Is_Created_Successfully()
+    {
+        //Arrange
+
+
+        //Act
+
+
+        //Assert
+
+    }
+}
