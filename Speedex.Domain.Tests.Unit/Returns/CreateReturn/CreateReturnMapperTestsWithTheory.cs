4a749fc14cf9be2b90652ac86e2f0cc466a6c4e4
Hugo Tachoires <hugo.tachoires@cdbdx.biz>
2025-01-28 10:21:00 +0100

    test(return): add theory test template

---
+++ Speedex.Domain.Tests.Unit/Returns/CreateReturn/CreateReturnMapperTestsWithTheory.cs
@@ -0,0 +1,57 @@
+using Speedex.Domain.Orders;
+using Speedex.Domain.Parcels;
+using Speedex.Domain.Returns;
+using Speedex.Domain.Returns.UseCases.CreateReturn;
+
+namespace Speedex.Domain.Tests.Unit.Returns.CreateReturn;
+
+public class CreateReturnMapperWithTheoryTests
+{
+    // [Theory]
+    public void ToReturn_Should_Create_Return_With_Specific_OrderId_When_Mapping_Create_Return_Command()
+    {
+        //Arrange
+        //TODO: Instantiate CreateReturnCommand, used for your test (in act part)
+
+        //Act
+        // var createdReturn = command.ToReturn();
+
+        //Assert
+        //TODO: add the appropriate assertion to check return status
+        //Assert.Equal ?
+        //Assert.Null ?
+        //Assert.NotNull ?
+    }
+
+    // [Theory]
+    public void ToReturn_Should_Create_Return_With_Specific_ParcelId_When_Mapping_Create_Return_Command()
+    {
+        //Arrange
+        //TODO: Instantiate CreateReturnCommand, used for your test (in act part)
+
+        //Act
+        // var createdReturn = command.ToReturn();
+
+        //Assert
+        //TODO: add the appropriate assertion to check return status
+        //Assert.Equal ?
+        //Assert.Null ?
+        //Assert.NotNull ?
+    }
+
+    // [Theory]
+    public void ToReturn_Should_Create_Return_With_Specific_ProductId_When_Mapping_Create_Return_Command()
+    {
+        //Arrange
+        //TODO: Instantiate CreateReturnCommand, used for your test (in act part)
+
+        //Act
+        // var createdReturn = command.ToReturn();
+
+        //Assert
+        //TODO: add the appropriate assertion to check return status
+        //Assert.Equal ?
+        //Assert.Null ?
+        //Assert.NotNull ?
+    }
+}
