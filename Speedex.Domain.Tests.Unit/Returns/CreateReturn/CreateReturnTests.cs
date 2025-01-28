75a85f4d832c1bd9a7237e40f01691adcab4c70e
Hugo Tachoires <hugo.tachoires@gmail.com>
2025-01-28 11:45:31 +0100

    test: add template code

--- Speedex.Domain.Tests.Unit/Returns/CreateReturn/CreateReturnTests.cs
+++ Speedex.Domain.Tests.Unit/Returns/CreateReturn/CreateReturnTests.cs
@@ -5,13 +5,23 @@ public class CreateReturnTests
     [Fact]
     public void Handle_Should_Return_Success_Result_When_Return_Is_Created_Successfully()
     {
-        //Arrange
-
-
-        //Act
-
-
-        //Assert
-
+        // Arrange
+        //TODO: make it work for create return command
+        // var orderRepository = Substitute.For<IOrderRepository>();
+        //
+        // orderRepository
+        //     .UpsertOrder(Arg.Any<Order>())
+        //     .Returns(new UpsertOrderResult { Status = UpsertOrderResult.UpsertStatus.Success });
+        //
+        // var command = ACreateOrderCommand.Build();
+        //
+        // var handler = new CreateOrderCommandHandler(orderRepository);
+        //
+        // // Act
+        // var result = handler.Handle(command);
+        //
+        // // Assert
+        // Assert.True(result.Success);
+        // Assert.NotNull(result.OrderId);
     }
 }
