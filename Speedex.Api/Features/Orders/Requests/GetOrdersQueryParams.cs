065025e28d20e7769953b42a21403638bdb098f3
Zachary <zachary.tanji@etu.u-bordeaux.fr>
2025-02-11 16:48:21 +0100

    feat: Query Search By CustomerEmail #US-03

--- Speedex.Api/Features/Orders/Requests/GetOrdersQueryParams.cs
+++ Speedex.Api/Features/Orders/Requests/GetOrdersQueryParams.cs
@@ -5,5 +5,6 @@ public record GetOrdersQueryParams
     public string? OrderId { get; init; }
     public string? ProductId { get; init; }
     public int? PageIndex { get; init; }
+    public string? CustomerEmail { get; set; } 
     public int? PageSize { get; init; }
 }
