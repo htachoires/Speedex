dd6e12b9a09f5ae046ff99db111c3139792dda33
Hugo Tachoires <hugo.tachoires@gmail.com>
2025-02-11 16:43:18 +0100

    feat: implement example US-04

--- Speedex.Domain/Orders/UseCases/CreateOrder/CreateOrderCommandHandler.cs
+++ Speedex.Domain/Orders/UseCases/CreateOrder/CreateOrderCommandHandler.cs
@@ -2,6 +2,7 @@ using FluentValidation;
 using Speedex.Domain.Commons;
 using Speedex.Domain.Orders.Repositories;
 using Speedex.Domain.Orders.Repositories.Dtos;
+using Speedex.Domain.Products.Repositories;
 
 namespace Speedex.Domain.Orders.UseCases.CreateOrder;
 
@@ -9,11 +10,16 @@ public class CreateOrderCommandHandler : ICommandHandler<CreateOrderCommand, Cre
 {
     private readonly IOrderRepository _orderRepository;
     private readonly IValidator<CreateOrderCommand> _commandValidator;
+    private readonly IProductRepository _productRepository;
 
-    public CreateOrderCommandHandler(IOrderRepository orderRepository, IValidator<CreateOrderCommand> commandValidator)
+    public CreateOrderCommandHandler(
+        IOrderRepository orderRepository,
+        IValidator<CreateOrderCommand> commandValidator,
+        IProductRepository productRepository)
     {
         _orderRepository = orderRepository;
         _commandValidator = commandValidator;
+        _productRepository = productRepository;
     }
 
     public async Task<CreateOrderResult> Handle(CreateOrderCommand command, CancellationToken cancellationToken = default)
@@ -36,6 +42,30 @@ public class CreateOrderCommandHandler : ICommandHandler<CreateOrderCommand, Cre
 
         //TODO: Check that weight in command is respected ( <30kg) for all containing products
         // hint use productRepository
+        double totalWeight = 0;
+        foreach (var commandProduct in command.Products)
+        {
+            var product = await _productRepository
+                .GetProductById(commandProduct.ProductId, CancellationToken.None);
+            totalWeight += product.Weight.ToKilograms().Value;
+        }
+
+        if (totalWeight > 30)
+        {
+            return new CreateOrderResult
+            {
+                Success = false,
+                Errors =
+                [
+                    new CreateOrderResult.ValidationError
+                    {
+                        Message = "Command weight is more than 30kg",
+                        Code = "Command_WeightExceeded_Error"
+                    }
+                ]
+            };
+        }
+
         var createdOrder = command.ToOrder();
 
         var result = _orderRepository.UpsertOrder(createdOrder);
